6fc7f785c80a44bd1e479317ce6c7877f8aef759
Georgy Chikhladze <georgiy.chikhladze@gmail.com>
2016-05-21 15:17:48 +0300

    Configurable port

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.info('Example app listening on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.info(`Server listening on port ${port}`);
 });
