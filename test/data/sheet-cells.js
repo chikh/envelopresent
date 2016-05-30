fa5a673198da70da14769bd0f760a19f547343b1
Georgy Chikhladze <georgiy.chikhladze@gmail.com>
2016-05-31 01:39:56 +0300

    Stub cells data

---
+++ test/data/sheet-cells.js
@@ -0,0 +1,59 @@
+module.exports = [{
+  'value': '100',
+  'numericValue': 100,
+  'valueForSave': '100',
+  'id': 'https://spreadsheets.google.com/feeds/cells/PXxhWU_nidScr1Q2C-KTgdk4/qmv1z9i/R2C2',
+  'row': 2,
+  'col': 2,
+  'batchId': 'R2C2',
+  '_links': [],
+  '_numericValue': 100,
+  '_value': '100'
+}, {
+  'value': 'Lois',
+  'valueForSave': 'Lois',
+  'id': 'https://spreadsheets.google.com/feeds/cells/PXxhWU_nidScr1Q2C-KTgdk4/qmv1z9i/R2C3',
+  'row': 2,
+  'col': 3,
+  'batchId': 'R2C3',
+  '_links': [],
+  '_value': 'Lois'
+}, {
+  'value': 'Victor',
+  'valueForSave': 'Victor',
+  'id': 'https://spreadsheets.google.com/feeds/cells/PXxhWU_nidScr1Q2C-KTgdk4/qmv1z9i/R3C3',
+  'row': 3,
+  'col': 3,
+  'batchId': 'R3C3',
+  '_links': [],
+  '_value': 'Victor'
+}, {
+  'value': 'Jennifer',
+  'valueForSave': 'Jennifer',
+  'id': 'https://spreadsheets.google.com/feeds/cells/PXxhWU_nidScr1Q2C-KTgdk4/qmv1z9i/R4C3',
+  'row': 4,
+  'col': 3,
+  'batchId': 'R4C3',
+  '_links': [],
+  '_value': 'Jennifer'
+}, {
+  'value': '150',
+  'numericValue': 150,
+  'valueForSave': '150',
+  'id': 'https://spreadsheets.google.com/feeds/cells/PXxhWU_nidScr1Q2C-KTgdk4/qmv1z9i/R8C2',
+  'row': 8,
+  'col': 2,
+  'batchId': 'R8C2',
+  '_links': [],
+  '_numericValue': 150,
+  '_value': '150'
+}, {
+  'value': 'Amelie',
+  'valueForSave': 'Amelie',
+  'id': 'https://spreadsheets.google.com/feeds/cells/PXxhWU_nidScr1Q2C-KTgdk4/qmv1z9i/R8C3',
+  'row': 8,
+  'col': 3,
+  'batchId': 'R8C3',
+  '_links': [],
+  '_value': 'Amelie'
+}];
