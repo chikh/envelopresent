79d15f0dbcc4764cc974216b763d2294d09253ae
Georgy Chikhladze <georgiy.chikhladze@gmail.com>
2016-06-03 00:14:56 +0300

    Refactoring: better concerns segregation

--- routes/envelope-view.js
+++ routes/envelope-view.js
@@ -1,18 +1,9 @@
-module.exports = (googleSheetsProvider, calculator) => {
+module.exports = (sheetDataMiner) => {
   const router = require('express').Router();
 
-  const googleSheetInfo = {
-    email: process.env.GOOGLE_EMAIL,
-    privateKey: process.env.GOOGLE_PRIVATE_KEY,
-    googleSheetKey: process.env.GOOGLE_SHEET_ID
-  };
-
-  const googleSheet = googleSheetsProvider(googleSheetInfo);
-
   router.get('/', (req, res) => {
-    googleSheet.sheets
-      .then(sheets => calculator(sheets[1])
-      .then(sum => res.send('Сумма: ' + sum)))
+    sheetDataMiner.calculateSum()
+      .then(sum => res.send('Сумма: ' + sum))
       .catch(e => res.error(e));
   });
 
--- services/sheet-data-miner.js
+++ services/sheet-data-miner.js
@@ -2,7 +2,7 @@ const maxRows = 100;
 
 module.exports = (googleSheetsProvider) => ({
   calculateSum: () => googleSheetsProvider.cells({
-    sheet: 1, minRow: 2, maxRow: 2 + maxRows, minCol: 2, maxCol: 2
+    sheetNumber: 1, minRow: 2, maxRow: 2 + maxRows, minCol: 2, maxCol: 2
   }).then(cells => cells
     .filter(c => c.numericValue)
     .map(c => c.numericValue).reduce((sum, value) => sum + value, 0)
