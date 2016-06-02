b322c4506e9009d0959e3ed9ab7bf9f7565bf18c
Georgy Chikhladze <georgiy.chikhladze@gmail.com>
2016-06-03 00:59:43 +0300

    Simple view of handed over list

--- routes/envelope-view.js
+++ routes/envelope-view.js
@@ -2,9 +2,14 @@ module.exports = (sheetDataMiner) => {
   const router = require('express').Router();
 
   router.get('/', (req, res) => {
-    sheetDataMiner.calculateSum()
-      .then(sum => res.send('Сумма: ' + sum))
-      .catch(e => res.error(e));
+    Promise.all(
+      [sheetDataMiner.calculateSum(), sheetDataMiner.handedOverList()]
+    ).then(([sum, handedOver]) =>
+      res.send(
+        'Сдали: ' + handedOver.join(', ') + '<br>' +
+        'Сумма: ' + sum
+      )
+    ).catch(e => res.error(e));
   });
 
   return router;
